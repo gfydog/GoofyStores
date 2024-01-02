14b1f448e2cd0b76818c044348e26e93b0f06a93
rgmendezr <raul301095@gmail.com>
2024-01-02 11:37:44 -0600

    Delete configFinal.php

--- v1.0/config/configFinal.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("ROOT", 'http://localhost/GoofyStores/GoofyStores/v1.0/');
-define("SHORTROOT", '//localhost/GoofyStores/GoofyStores/v1.0/');
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'prueba1';
